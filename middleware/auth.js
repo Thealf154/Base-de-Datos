71ae655631a7bf47058ee8af05ec02a46bddc8ae
Alfredo Vanegas <thealf154@gmail.com>
2022-05-11 17:18:13 -0500

    Add security changes

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,19 +1,19 @@
 const jwt = require("jsonwebtoken");
 
-//This tries to get the authorization token form the user 
-module.exports = ((req, res, next) => {
-  try
-  {
-    const token = req.headers.authorization.split(" ")[1];
+//This tries to get the authorization token form the user
+module.exports = (req, res, next) => {
+  try {
+    const token =
+      req.body.token || req.query.token || req.headers["authorization"];
     //If the key gets decoded successfully you can store the key in decoded
     const decoded = jwt.verify(token, "debugkey");
     //With this, we can stop asking everytime for the token key
     req.user = decoded;
     //Calls the other routes, ej: /pokemon
     next();
+  } catch (error) {
+    return res
+      .status(401)
+      .json({ code: 401, message: req, error: error, });
   }
-  catch(error)
-  {
-    return res.status(401).json({ code: 401, message: "No tienes acceso", error: error})
-  }
-});
+};
