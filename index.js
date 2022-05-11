83bb9ccc47b1684e4150e2d5557767cdd88e3962
Alfredo Vanegas <thealf154@gmail.com>
2022-05-11 17:16:40 -0500

    Remove placeholder code

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 //Routes
 const students = require("./routes/students");
 const user = require("./routes/user");
+const subjects = require('./routes/subjects');
 //Middleware
 const notFound = require("./middleware/notFound");
 const index = require("./middleware/index");
@@ -17,8 +18,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.get("/", index);
 app.use("/user", user);
+app.use("/students", students);
+app.use("/subjects", subjects);
 
-app.use("/pokemon", students);
 app.use(notFound);
 
 //Load a local server
