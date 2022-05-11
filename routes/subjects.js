461144b36e93e9f1d60c9ac59f8db96f9adbd1a0
Alfredo Vanegas <thealf154@gmail.com>
2022-05-11 17:19:18 -0500

    Change some routes to receive data from database

--- routes/subjects.js
+++ routes/subjects.js
@@ -1,20 +1,18 @@
 const express = require("express");
-const students = express.Router();
+const subjects = express.Router();
 const db = require("../config/database");
 const auth = require("../middleware/auth");
 
-students.get("/", auth, async (req, res, next) => {
+subjects.get("/", auth, async (req, res, next) => {
   //This will wait for the database and won't execute
   //anything else until a response
-  const stdnt = await db.query(
-    "SELECT students.*, CONCAT(students.middle_name, ' ', students.last_name) as 'middle_and_last_name' FROM students"
-  );
+  const sbjct = await db.query("SELECT * FROM subjects");
   //Sends the database as a json, but only if the format is already in a json
-  return res.status(200).json({ code: 200, message: stdnt });
+  return res.status(200).json({ code: 200, message: sbjct });
 });
 
 //Expresión regular que acepta un grupo de tres números
-students.get("/:id([0-9]{1,3})", async (req, res, next) => {
+subjects.get("/:id([0-9]{1,3})", async (req, res, next) => {
   const pokemon = await db.query("SELECT * FROM pokemon");
   const id = req.params.id - 1;
   if (id >= 0 && id < 722) {
@@ -24,7 +22,7 @@ students.get("/:id([0-9]{1,3})", async (req, res, next) => {
 });
 
 //This only receives text requests
-students.get("/:name([A-Za-z]+)", async (req, res, next) => {
+subjects.get("/:name([A-Za-z]+)", async (req, res, next) => {
   const name = req.params.name;
   let pokemon = await db.query(
     `SELECT pok_name FROM pokemon WHERE pok_name LIKE '%${name}%'`
@@ -35,34 +33,18 @@ students.get("/:name([A-Za-z]+)", async (req, res, next) => {
 });
 
 /* Post, Delete, and other routes*/
-students.post("/", async (req, res, next) => {
-  const {
-    first_name,
-    middle_name,
-    last_name,
-    birth_date,
-    gender,
-    address,
-    faculty,
-    semester,
-    grade,
-  } = req.body;
+subjects.post("/", auth, async (req, res, next) => {
+  const { subject_name, semester, teacher_name, teacher_email, description } =
+    req.body;
 
-  if (
-    first_name &&
-    middle_name &&
-    last_name &&
-    birth_date &&
-    gender &&
-    address &&
-    faculty &&
-    semester &&
-    grade
-  ) {
+  if (subject_name && semester && teacher_name && teacher_email) {
     let query =
-      "INSERT INTO `students`(`first_name`, `middle_name`, `last_name`, `birth_date`, `gender`, `address`, `faculty`, `semester`, `grade`)";
-    query += ` VALUES ('${first_name}','${middle_name}','${last_name}','${birth_date}','${gender}','${address}','${faculty}','${semester}','${grade}')`;
-    console.log(query);
+      "INSERT INTO `subjects`(`subject_name`, `semester`, `teacher_name`, `teacher_email`, `description`) ";
+    if (description) {
+      query += `VALUES ('${subject_name}','${semester}','${teacher_name}','${teacher_email}','${description}')`;
+    } else {
+      query += `VALUES ('${subject_name}','${semester}','${teacher_name}','${teacher_email}', '')`;
+    }
     const rows = await db.query(query);
     if (rows.affectedRows == 1) {
       return res
@@ -76,8 +58,9 @@ students.post("/", async (req, res, next) => {
     .json({ code: 500, message: "Pokemon insertado sin éxito" });
 });
 
-students.delete("/:id([0-9]{1,3})", async (req, res, next) => {
-  const query = `DELETE FROM students WHERE pok_id=${req.params.id}`;
+subjects.delete("/:id([0-9]{1,3})", auth , async (req, res, next) => {
+  const query = `DELETE FROM subjects WHERE id=${req.params.id}`;
+  console.log('xd');
   const rows = await db.query(query);
 
   //This checks if only ONE column is affected
@@ -90,7 +73,7 @@ students.delete("/:id([0-9]{1,3})", async (req, res, next) => {
 });
 
 //PUT is for changing all the values of datacell
-students.put("/:id([0-9]{1,3})", async (req, res, next) => {
+subjects.put("/:id([0-9]{1,3})", async (req, res, next) => {
   const { pok_name, pok_height, pok_weight, pok_base_experience } = req.body;
 
   if (pok_name && pok_height && pok_base_experience && pok_weight) {
@@ -110,7 +93,7 @@ students.put("/:id([0-9]{1,3})", async (req, res, next) => {
     .json({ code: 500, message: "Pokemon actulizado sin éxito" });
 });
 
-students.patch("/:id([0-9]{1,3})", async (req, res, next) => {
+subjects.patch("/:id([0-9]{1,3})", async (req, res, next) => {
   const { pok_name, pok_height, pok_weight, pok_base_experience } = req.body;
 
   if (req.body.pok_name) {
@@ -129,4 +112,4 @@ students.patch("/:id([0-9]{1,3})", async (req, res, next) => {
   return res.status(500).json({ code: 500, message: "No existe el Pokemon" });
 });
 
-module.exports = students;
+module.exports = subjects;
